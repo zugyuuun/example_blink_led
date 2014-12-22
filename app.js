b751e8b2484a0ea04e6c68d8a900e9f4e07ebf9e
k.yamasaki <k.yamasaki@bunnyhop.jp>
2014-12-22 18:06:53 +0900

    [add]Blink LED Example

---
+++ app.js
@@ -0,0 +1,21 @@
+ZGN(function()
+{
+  // 17番ピンで動作させます
+  var ledPin = '17';
+
+  // TerminalのGPIOインスタンスを取得します
+  var gpio = ZGN.term('1').gpio;
+
+  // 指定ピンを出力に設定
+  gpio.pinMode(ledPin, ZGN.OUTPUT);
+
+  // ONボタンをクリック
+  $(document).on('click', '#on', function() {
+    gpio.digitalWrite(ledPin, ZGN.HIGH); // 点灯
+  });
+
+  // OFFボタンをクリック
+  $(document).on('click', '#off', function() {
+    gpio.digitalWrite(ledPin, ZGN.LOW); // 消灯
+  });
+});
